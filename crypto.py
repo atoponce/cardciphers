e52d3c351576ed9832405a79b4076ebdd94be604
Aaron Toponce <aaron.toponce@gmail.com>
2015-06-29 13:34:15 -0600

    Support keyed deck and fix providing a passphrase
    keyed deck must be digits. does not yet support text (AC, 2C, etc)
    also fixed which character set is pulled from when using a passphrase

--- crypto.py
+++ crypto.py
@@ -9,16 +9,49 @@ group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-d', '--decrypt', help='Decrypt a message')
 group.add_argument('-e', '--encrypt', help='Encrypt a message')
 parser.add_argument('-p', '--passphrase', help='Private passphrase to key deck')
+parser.add_argument('-k', '--key', help='Private comma-separated deck order.')
 args = parser.parse_args()
 
+last = -1
+coincidences = 0
 alg = talon.Cipher()
 deck = [i for i in xrange(1,53)]
+
+if args.key:
+    deck = []
+    cards = args.key.split(',')
+
+    if not ',' in args.key:
+        parser.error('All 52 cards must be comma-separated.')
+
+    if not args.key.replace(' ','').replace(',','').isdigit():
+        parser.error('The deck must uniquely comprise of digits 1-52.')
+
+    # bulid deck
+    for card in cards:
+        deck.append(int(card))
+
+    # more tests
+    if len(deck) != 52:
+        parser.error('The deck must be exactly 52 cards in total.')
+
+    if sorted(deck)[0] < 1 or sorted(deck)[51] > 52:
+        parser.error('The deck must use uniquely the values 1-52 inclusively.')
+
+    for card in deck:
+        out = card
+        if out == last:
+            coincidences += 1
+        last = out
+
+    if coincidences:
+        parser.error('The deck must contain all unique values of 1-52.')
 
 if args.passphrase:
     passphrase = args.passphrase.upper()
     for char in passphrase:
         alg.mix_deck(deck)
-        deck = alg.count_cut(deck, encoder.clist.index(char)+1)
+        deck = alg.count_cut(deck, encoder.plist.index(char)+1)
 
 if args.encrypt:
     plaintext = args.encrypt.upper()
