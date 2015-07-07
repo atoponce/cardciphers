043b57bd8b825f257c5d4cbb61148b98f278ea54
Aaron Toponce <aaron.toponce@gmail.com>
2015-07-07 17:08:12 -0600

    show starting point of unkeyed and keyed decks

---
+++ card_chameleon/beginnings.py
@@ -0,0 +1,32 @@
+import card_chameleon
+
+c = card_chameleon.Cipher()
+passphrase = 'CARDCIPHERCARDCIPHER'
+deck = [i for i in xrange(1,53)]
+cards = (
+    'AC','2C','3C','4C','5C','6C','7C','8C','9C','TC','JC','QC','KC',
+    'AD','2D','3D','4D','5D','6D','7D','8D','9D','TD','JD','QD','KD',
+    'AH','2H','3H','4H','5H','6H','7H','8H','9H','TH','JH','QH','KH',
+    'AS','2S','3S','4S','5S','6S','7S','8S','9S','TS','JS','QS','KS'
+    )
+
+print "Default unkeyed deck:"
+for card in deck:
+    print cards[card-1],
+
+print "\n"
+
+c.prepare_deck(deck)
+
+print "Unkeyed Card-Chameleon deck:"
+for card in deck:
+    print cards[card-1],
+
+print "\n"
+
+for char in passphrase:
+    c.prng(deck, char, iv=True)
+
+print "Keyed Card-Chameleon deck:"
+for card in deck:
+    print cards[card-1],
