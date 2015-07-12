574a054836f48928b01518c60062141449395e0d
Aaron Toponce <aaron.toponce@gmail.com>
2015-07-12 08:04:58 -0600

    re-add 5 character initialization vector

--- solitaire/crypto.py
+++ solitaire/crypto.py
@@ -57,7 +57,7 @@ if args.passphrase:
 
 if args.encrypt:
     plaintext = args.encrypt.upper()
-    encrypted = encoder.encrypt(plaintext, alg, deck, 0)
+    encrypted = encoder.encrypt(plaintext, alg, deck, 5)
 
     ciphertext = ''
     for index, char in enumerate(encrypted):
@@ -69,7 +69,7 @@ if args.encrypt:
 
 elif args.decrypt:
     ciphertext = args.decrypt.replace(' ','')
-    decrypted = encoder.decrypt(ciphertext, alg, deck, 0)
+    decrypted = encoder.decrypt(ciphertext, alg, deck, 5)
 
     plaintext = ''
     for char in decrypted:
